0c523d63e08746222883de59e2cafb34ebec9c5b
Brian Matovu <mtvbrianking@gmail.com>
2018-09-16 16:44:40 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Demonstrating Android Shimmer Effect
+
+Adapted from AndroidHive [tutorial](https://www.androidhive.info/2018/01/android-content-placeholder-animation-like-facebook-using-shimmer/)
---
+++ app/src/main/java/com/bmatovu/demo/shimmer/KitchenActivity.java
@@ -0,0 +1,13 @@
+package com.bmatovu.demo.shimmer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KitchenActivity extends AppCompatActivity {
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_kitchen);
+   }
+}
